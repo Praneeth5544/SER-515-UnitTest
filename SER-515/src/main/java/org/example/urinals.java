9396638911545b9cabdc7cd717dbc94e3e664f1f
Praneeth5544 <pkancha2@asu.edu>
2022-10-28 00:54:40 -0700

    SER-51updated

--- SER-515/src/main/java/org/example/urinals.java
+++ SER-515/src/main/java/org/example/urinals.java
@@ -33,6 +33,14 @@ public class urinals {
             }
             for(int i=0 ; i<arr.length;i++)
             {
+                if(arr.length==1)
+                {
+                    if(arr[i]=='0')
+                    {
+                        arr[i]=1;
+                        c2=c2+1;
+                    }
+                }
                 if(i==0)
                 {
                     if(arr[i]=='0'&& arr[i+1]=='0')
@@ -127,7 +135,6 @@ public class urinals {
         }
         else {
             int z=u.openFile();
-            System.out.println(z);
         }
     }
 }
