29bd47ac0b1559bed89e4e7e6faa39783668e595
Praneeth5544 <pkancha2@asu.edu>
2022-10-28 00:47:05 -0700

    Urinals updated

--- urinals.java
+++ urinals.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.io.*;
-import java.util.List;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class urinals {
     public boolean goodString(String str)
@@ -85,49 +82,13 @@ public class urinals {
             return -1;
         }
     }
-    public int openFile()
-    {
-        int i = 0,j=0;
-        try {
-            FileReader fr = new FileReader("C:\\Users\\hp\\Desktop\\SER 515\\Assignment-2-Ind\\SER-515-Praneeth-Reddy-K\\TextFiles\\BuyerInfo.txt");
-            FileWriter fWriter = new FileWriter("C:\\Users\\hp\\Desktop\\SER 515\\Assignment-2-Ind\\SER-515-Praneeth-Reddy-K\\javafiles\\SER-515-UnitTest\\rule1.txt");
-            //File f=new File();
-            BufferedReader br = new BufferedReader(fr);
-            BufferedWriter bw= new BufferedWriter(fWriter);
-            String st;
-            while ((st = br.readLine()) != null) {
-                StringTokenizer s = new StringTokenizer(st, " ");
-                while (s.hasMoreTokens())
-                {
-                    i=countUrinals(s.nextToken());
-                    bw.write(i);
-                    bw.newLine();
-                }
-            }
-        }
-        catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("File created Successfully");
-        return i;
-    }
     public static void main(String[] args) throws Exception
     {
         urinals u= new urinals();
         Scanner sc = new Scanner(System.in);
-        System.out.println("For Input from keyboard enter 0 For Input from files enter 1");
-        int opt=sc.nextInt();
-        if(opt==0) {
-            System.out.println("Enter The string");
-            String str = sc.next();
-            int x = u.countUrinals(str);
-            System.out.println(x);
-        }
-        else {
-            int z=u.openFile();
-            System.out.println(z);
-        }
+        System.out.println("Enter The string");
+        String str= sc.nextLine();
+        int x= u.countUrinals(str);
+        System.out.println(x);
     }
 }
